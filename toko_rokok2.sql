21a7db873277aa37538cdac82156a7251ec5f9b8
AgustinaPutriMonika <112864613+AgustinaPutriMonika@users.noreply.github.com>
2023-05-03 17:52:36 +0700

    Update toko_rokok2.sql

--- toko_rokok2.sql
+++ toko_rokok2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2023 at 10:29 AM
+-- Generation Time: May 03, 2023 at 12:51 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -38,6 +38,28 @@ CREATE TABLE `absensi` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `distrik`
+--
+
+CREATE TABLE `distrik` (
+  `kd_distrik` varchar(10) NOT NULL,
+  `nama_distrik` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `distrik`
+--
+
+INSERT INTO `distrik` (`kd_distrik`, `nama_distrik`) VALUES
+('BTL', 'Bantul'),
+('JOG', 'Jogja'),
+('KLT', 'Klaten'),
+('KTS', 'Kartasura'),
+('SRG', 'Sragen');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `karyawan`
 --
 
@@ -60,6 +82,39 @@ INSERT INTO `karyawan` (`kd_karyawan`, `nama_karyawan`, `level`, `jabatan`, `no_
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `masuk_spo`
+--
+
+CREATE TABLE `masuk_spo` (
+  `kd_masuk_spo` int(11) NOT NULL,
+  `nama_karyawan` varchar(50) NOT NULL,
+  `kd_jual` int(11) NOT NULL,
+  `kd_toko` int(11) NOT NULL,
+  `kd_distrik` varchar(10) NOT NULL,
+  `routing` varchar(5) NOT NULL,
+  `tanggal_masuk` datetime NOT NULL DEFAULT current_timestamp(),
+  `tanggal_keluar` datetime NOT NULL DEFAULT current_timestamp(),
+  `keterangan` varchar(255) NOT NULL,
+  `gambar_toko` varchar(255) NOT NULL,
+  `latitude` varchar(50) NOT NULL,
+  `longitude` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `masuk_spo`
+--
+
+INSERT INTO `masuk_spo` (`kd_masuk_spo`, `nama_karyawan`, `kd_jual`, `kd_toko`, `kd_distrik`, `routing`, `tanggal_masuk`, `tanggal_keluar`, `keterangan`, `gambar_toko`, `latitude`, `longitude`) VALUES
+(3, '', 56, 52, 'SRG', '8', '2023-05-01 16:16:34', '2023-05-31 16:16:34', '', '', '-7.7543187', '110.4235213'),
+(4, '', 57, 48, 'KLT', '5', '2023-05-01 16:55:15', '2023-05-31 16:55:15', '', '', '-7.7529088', '110.2381056'),
+(5, '', 58, 50, 'KTS', '7', '2023-05-01 16:56:51', '2023-05-26 16:56:51', '', '', '-7.7529088', '110.2381056'),
+(6, '', 59, 54, 'BTL', '9', '2023-05-01 16:59:22', '2023-05-16 16:59:22', '', '', '', ''),
+(7, 'Agustina', 60, 48, 'KLT', '10', '2023-05-03 01:38:38', '2023-05-18 01:38:38', 'apayaaa', '', '', ''),
+(8, '', 64, 51, 'SRG', '4', '2023-05-03 01:50:52', '2023-05-03 01:54:52', '', '', '-7.7543142', '110.4235181');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `masuk_toko`
 --
 
@@ -67,25 +122,23 @@ CREATE TABLE `masuk_toko` (
   `id_masuk` int(11) NOT NULL,
   `nama_karyawan` varchar(50) NOT NULL,
   `kd_jual` int(11) NOT NULL,
-  `id_toko` int(11) NOT NULL,
-  `tanggal_masuk` datetime NOT NULL DEFAULT current_timestamp()
+  `kd_toko` int(11) NOT NULL,
+  `kd_distrik` varchar(10) NOT NULL,
+  `routing` varchar(2) NOT NULL,
+  `tanggal_masuk` datetime NOT NULL DEFAULT current_timestamp(),
+  `jenis_kunjungan` varchar(25) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `gambar_toko` varchar(255) NOT NULL,
+  `latitude` varchar(50) NOT NULL,
+  `longitude` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `masuk_toko`
 --
 
-INSERT INTO `masuk_toko` (`id_masuk`, `nama_karyawan`, `kd_jual`, `id_toko`, `tanggal_masuk`) VALUES
-(30, 'Agustina', 35, 33, '2023-04-27 07:23:56'),
-(34, 'Mark Lee', 39, 37, '2023-04-27 20:11:46'),
-(35, 'Haruto', 40, 38, '2023-04-27 20:13:02'),
-(36, 'Putri', 41, 39, '2023-04-27 20:13:53'),
-(37, 'Jaemin', 42, 40, '2023-04-27 20:15:00'),
-(38, 'NCT', 43, 41, '2023-04-27 20:15:48'),
-(39, '', 44, 42, '2023-04-28 19:25:33'),
-(40, '', 45, 43, '2023-04-28 20:49:59'),
-(41, '', 46, 44, '2023-04-28 20:51:09'),
-(42, '', 47, 45, '2023-04-29 15:18:57');
+INSERT INTO `masuk_toko` (`id_masuk`, `nama_karyawan`, `kd_jual`, `kd_toko`, `kd_distrik`, `routing`, `tanggal_masuk`, `jenis_kunjungan`, `keterangan`, `gambar_toko`, `latitude`, `longitude`) VALUES
+(48, '', 53, 56, 'JOG', '1', '2023-05-01 12:02:04', 'io', '', '', '-7.7529088', '110.2381056');
 
 -- --------------------------------------------------------
 
@@ -104,24 +157,54 @@ CREATE TABLE `produk` (
   `KK` int(11) NOT NULL,
   `R12` int(11) NOT NULL,
   `R16` int(11) NOT NULL,
-  `kd_jual` int(11) NOT NULL
+  `kd_jual` int(11) NOT NULL,
+  `kd_stok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`B12`, `B16`, `B20`, `BB12`, `BB16`, `BICE16`, `KC`, `KK`, `R12`, `R16`, `kd_jual`) VALUES
-(3, 5, 7, 2, 8, 0, 0, 0, 0, 0, 35),
-(0, 0, 0, 4, 0, 0, 7, 0, 0, 0, 39),
-(5, 0, 0, 0, 0, 0, 0, 3, 0, 0, 40),
-(3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 41),
-(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42),
-(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 43),
-(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 44),
-(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45),
-(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 46),
-(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47);
+INSERT INTO `produk` (`B12`, `B16`, `B20`, `BB12`, `BB16`, `BICE16`, `KC`, `KK`, `R12`, `R16`, `kd_jual`, `kd_stok`) VALUES
+(0, 0, 5, 0, 6, 0, 0, 0, 0, 0, 53, 0),
+(23, 0, 0, 15, 0, 0, 0, 0, 0, 0, 54, 0),
+(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 0),
+(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56, 0),
+(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 57, 0),
+(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 58, 0),
+(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 59, 0),
+(0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 60, 0),
+(0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 61, 0),
+(0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 62, 0),
+(0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 63, 0),
+(0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 64, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_gudang_kecil`
+--
+
+CREATE TABLE `stok_gudang_kecil` (
+  `kd_stok` int(11) NOT NULL,
+  `B12` int(11) NOT NULL,
+  `B16` int(11) NOT NULL,
+  `B20` int(11) NOT NULL,
+  `BB12` int(11) NOT NULL,
+  `BB16` int(11) NOT NULL,
+  `BICE16` int(11) NOT NULL,
+  `KC` int(11) NOT NULL,
+  `KK` int(11) NOT NULL,
+  `R12` int(11) NOT NULL,
+  `R16` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stok_gudang_kecil`
+--
+
+INSERT INTO `stok_gudang_kecil` (`kd_stok`, `B12`, `B16`, `B20`, `BB12`, `BB16`, `BICE16`, `KC`, `KK`, `R12`, `R16`) VALUES
+(1, 95, 150, 277, 200, 96, 100, 160, 100, 94, 60);
 
 -- --------------------------------------------------------
 
@@ -130,32 +213,25 @@ INSERT INTO `produk` (`B12`, `B16`, `B20`, `BB12`, `BB16`, `BICE16`, `KC`, `KK`,
 --
 
 CREATE TABLE `toko` (
-  `id_toko` int(11) NOT NULL,
+  `kd_toko` int(11) NOT NULL,
   `nama_toko` varchar(100) NOT NULL,
-  `distrik` varchar(100) NOT NULL,
-  `routing` varchar(15) NOT NULL,
-  `jenis_kunjungan` varchar(25) NOT NULL,
-  `keterangan` varchar(255) NOT NULL,
-  `gambar_toko` varchar(255) NOT NULL,
-  `latitude` varchar(50) NOT NULL,
-  `longitude` varchar(50) NOT NULL
+  `kd_distrik` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `toko`
 --
 
-INSERT INTO `toko` (`id_toko`, `nama_toko`, `distrik`, `routing`, `jenis_kunjungan`, `keterangan`, `gambar_toko`, `latitude`, `longitude`) VALUES
-(33, 'Abadi', 'Magelang', '01', 'roc', '', 'Gochujang Hot Pepper Paste - Haechandle 3kg.jpg', '-7.754232', '110.4235226'),
-(37, 'SM', 'Seoul', '14', 'rot', '', 'WORKSHOP GURU INFORMATIKA SMP KABUPATEN SLEMAN (2).png', '-7.749632', '110.3986688'),
-(38, 'YG', 'Seoul', '12', 'r2w', '', 'Logo_Google_Org-removebg-preview.png', '-7.754187', '110.4235255'),
-(39, 'Monika', 'sleman', '01', 'roc', '', '', '-7.754189', '110.423524'),
-(40, 'Ruko', 'Magelang', 'sleman', 'rot', '', '', '-7.7542984', '110.423519'),
-(41, 'Dream', 'Bantul', '12', 'rot', '', '', '-7.7543213', '110.423516'),
-(42, '', '', '', 'io', '', '', '-7.7856768', '110.3757312'),
-(43, '', '', '', 'io', '', 'WORKSHOP GURU INFORMATIKA SMP KABUPATEN SLEMAN (6).png', '-7.764381', '110.4255206'),
-(44, '', '', '', 'io', '', '', '-7.7644332', '110.42553'),
-(45, '', '', '', 'io', '', '', '-7.7627827', '110.4143039');
+INSERT INTO `toko` (`kd_toko`, `nama_toko`, `kd_distrik`) VALUES
+(48, 'Sinar Sakti', 'KLT'),
+(49, 'Ali Jaya', 'KLT'),
+(50, 'Jodo', 'KTS'),
+(51, 'Sumber Rejeki', 'SRG'),
+(52, 'Wijaya Tani', 'SRG'),
+(53, 'Azqza', 'BTL'),
+(54, 'Bpk Gatot', 'BTL'),
+(55, 'Enggal Jaya', 'JOG'),
+(56, 'Ana', 'JOG');
 
 --
 -- Indexes for dumped tables
@@ -169,10 +245,26 @@ ALTER TABLE `absensi`
   ADD KEY `kd_karyawan` (`kd_karyawan`);
 
 --
+-- Indexes for table `distrik`
+--
+ALTER TABLE `distrik`
+  ADD PRIMARY KEY (`kd_distrik`);
+
+--
 -- Indexes for table `karyawan`
 --
 ALTER TABLE `karyawan`
   ADD UNIQUE KEY `kd_karyawan` (`kd_karyawan`);
+
+--
+-- Indexes for table `masuk_spo`
+--
+ALTER TABLE `masuk_spo`
+  ADD PRIMARY KEY (`kd_masuk_spo`),
+  ADD KEY `nama_karyawan` (`nama_karyawan`),
+  ADD KEY `kd_jual` (`kd_jual`),
+  ADD KEY `kd_toko` (`kd_toko`),
+  ADD KEY `kd_distrik` (`kd_distrik`);
 
 --
 -- Indexes for table `masuk_toko`
@@ -181,43 +273,59 @@ ALTER TABLE `masuk_toko`
   ADD PRIMARY KEY (`id_masuk`),
   ADD KEY `kd_karyawan` (`nama_karyawan`),
   ADD KEY `kd_produk` (`kd_jual`),
-  ADD KEY `id_toko` (`id_toko`),
+  ADD KEY `id_toko` (`kd_toko`),
   ADD KEY `kd_jual` (`kd_jual`),
-  ADD KEY `kd_karyawan_2` (`nama_karyawan`);
+  ADD KEY `kd_karyawan_2` (`nama_karyawan`),
+  ADD KEY `kd_toko` (`kd_toko`),
+  ADD KEY `kd_distrik` (`kd_distrik`);
 
 --
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
-  ADD PRIMARY KEY (`kd_jual`);
+  ADD PRIMARY KEY (`kd_jual`),
+  ADD KEY `kd_stok` (`kd_stok`);
+
+--
+-- Indexes for table `stok_gudang_kecil`
+--
+ALTER TABLE `stok_gudang_kecil`
+  ADD PRIMARY KEY (`kd_stok`);
 
 --
 -- Indexes for table `toko`
 --
 ALTER TABLE `toko`
-  ADD PRIMARY KEY (`id_toko`);
+  ADD PRIMARY KEY (`kd_toko`),
+  ADD KEY `kd_distrik` (`kd_distrik`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `masuk_spo`
+--
+ALTER TABLE `masuk_spo`
+  MODIFY `kd_masuk_spo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `masuk_toko`
 --
 ALTER TABLE `masuk_toko`
-  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `kd_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `kd_jual` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `toko`
 --
 ALTER TABLE `toko`
-  MODIFY `id_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `kd_toko` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
@@ -230,11 +338,26 @@ ALTER TABLE `absensi`
   ADD CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`kd_karyawan`) REFERENCES `karyawan` (`kd_karyawan`);
 
 --
+-- Constraints for table `masuk_spo`
+--
+ALTER TABLE `masuk_spo`
+  ADD CONSTRAINT `masuk_spo_ibfk_1` FOREIGN KEY (`kd_toko`) REFERENCES `toko` (`kd_toko`),
+  ADD CONSTRAINT `masuk_spo_ibfk_2` FOREIGN KEY (`kd_jual`) REFERENCES `produk` (`kd_jual`),
+  ADD CONSTRAINT `masuk_spo_ibfk_3` FOREIGN KEY (`kd_distrik`) REFERENCES `distrik` (`kd_distrik`);
+
+--
 -- Constraints for table `masuk_toko`
 --
 ALTER TABLE `masuk_toko`
-  ADD CONSTRAINT `masuk_toko_ibfk_1` FOREIGN KEY (`id_toko`) REFERENCES `toko` (`id_toko`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `masuk_toko_ibfk_3` FOREIGN KEY (`kd_jual`) REFERENCES `produk` (`kd_jual`);
+  ADD CONSTRAINT `masuk_toko_ibfk_1` FOREIGN KEY (`kd_toko`) REFERENCES `toko` (`kd_toko`),
+  ADD CONSTRAINT `masuk_toko_ibfk_3` FOREIGN KEY (`kd_jual`) REFERENCES `produk` (`kd_jual`),
+  ADD CONSTRAINT `masuk_toko_ibfk_4` FOREIGN KEY (`kd_distrik`) REFERENCES `distrik` (`kd_distrik`);
+
+--
+-- Constraints for table `toko`
+--
+ALTER TABLE `toko`
+  ADD CONSTRAINT `toko_ibfk_1` FOREIGN KEY (`kd_distrik`) REFERENCES `distrik` (`kd_distrik`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
