24e56a6fc7adb816cecae216f532114ee77674f2
AgustinaPutriMonika <112864613+AgustinaPutriMonika@users.noreply.github.com>
2023-05-07 23:34:10 +0700

    coba tambah stok di gudang besar

--- function.php
+++ function.php
@@ -1,18 +1,12 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","toko_rokok2");
+$koneksi = mysqli_connect("localhost","root","","toko_rokok3");
 
 // Check connection
 if (mysqli_connect_errno()){
  echo "Koneksi database gagal : " . mysqli_connect_error();
 }
 
-//Menambah barang baru
-if(isset($_POST['addnew'])){
-    $namasales = $_POST["namasales"];
-    $namatoko = $_POST["toko"];
-    $distrik = $_POST["distrik"];
-    $routing = $_POST["routing"];
-    $jkunjungan = $_POST["jkunjungan"];
+if(isset($_POST['updatestok'])){
     $b20 = $_POST["b20"];
     $b16 = $_POST["b16"];
     $b12 = $_POST["b12"];
@@ -23,89 +17,88 @@ if(isset($_POST['addnew'])){
     $bb16 = $_POST["bb16"];
     $bb12 = $_POST["bb12"];
     $bice = $_POST["bice"];
-    $keterangan = $_POST["keterangan"];
-    $latitude = $_POST["latitude"];
-    $longitude = $_POST["longitude"];
-
-    $addtoproduk = mysqli_query($koneksi, "INSERT INTO produk VALUES ('$b12', '$b16', '$b20', '$bb12', '$bb16', '$bice', '$kc', '$kk', '$r12', '$r16','','')");
-
-    $getidjual = mysqli_query($koneksi, "SELECT kd_jual FROM produk ORDER BY kd_jual DESC");
-    $rowidjual = mysqli_fetch_array($getidjual);
-    $idjual = $rowidjual['kd_jual'];
-
-    $folder = './produk/';
-    $name_p = $_FILES['foto']['name'];
-    $sumber_p = $_FILES['foto']['tmp_name'];
-    move_uploaded_file($sumber_p, $folder.$name_p);
-    date_default_timezone_set('Asia/Jakarta');
-    $tanggal = date('Y-m-d H:i:s');
-    $addmasuk = mysqli_query($koneksi, "INSERT INTO masuk_toko VALUES ('', '$namasales', '$idjual', '$namatoko', '$distrik', '$routing','$tanggal', '$jkunjungan', '$keterangan', '$name_p', '$latitude', '$longitude')");
 
     // update stok
-    $cekstok = mysqli_query($koneksi, "SELECT * FROM stok_gudang_kecil");
-    $ambildatanya = mysqli_fetch_array($cekstok);
-    $stokb20 = $ambildatanya['B20'];
-    $tambahstokb20 = $stokb20 - intval($b20);
+    $cekb20 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='B20'");
+    $ambildatanya = mysqli_fetch_array($cekb20);
+    $stokb20 = $ambildatanya['stok'];
+    $tambahstokb20 = $stokb20 + intval($b20);
 
-    $stokb16 = $ambildatanya['B16'];
-    $tambahstokb16 = $stokb16 - intval($b16);
+    $cekb16 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='B16'");
+    $ambildatanya = mysqli_fetch_array($cekb16);
+    $stokb16 = $ambildatanya['stok'];
+    $tambahstokb16 = $stokb16 + intval($b16);
 
-    $stokb12 = $ambildatanya['B12'];
-    $tambahstokb12 = $stokb12 - intval($b12);
+    $cekb12 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='B12'");
+    $ambildatanya = mysqli_fetch_array($cekb12);
+    $stokb12 = $ambildatanya['stok'];
+    $tambahstokb12 = $stokb12 + intval($b12);
 
-    $stokr16 = $ambildatanya['R16'];
-    $tambahstokr16 = $stokr16 - intval($r16);
+    $cekr16 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='R16'");
+    $ambildatanya = mysqli_fetch_array($cekr16);
+    $stokr16 = $ambildatanya['stok'];
+    $tambahstokr16 = $stokr16 + intval($r16);
 
-    $stokr20 = $ambildatanya['R12'];
-    $tambahstokr20 = $stokr20 - intval($r12);
+    $cekr12 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='R12'");
+    $ambildatanya = mysqli_fetch_array($cekr12);
+    $stokr12 = $ambildatanya['stok'];
+    $tambahstokr12 = $stokr12 + intval($r12);
 
-    $stokkk = $ambildatanya['KK'];
-    $tambahstokkk = $stokkk - intval($kk);
+    $cekkk = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='KK'");
+    $ambildatanya = mysqli_fetch_array($cekkk);
+    $stokkk = $ambildatanya['stok'];
+    $tambahstokkk = $stokkk + intval($kk);
 
-    $stokkc = $ambildatanya['KC'];
-    $tambahstokkc = $stokkc - intval($kc);
+    $cekkc = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='KC'");
+    $ambildatanya = mysqli_fetch_array($cekkc);
+    $stokkc = $ambildatanya['stok'];
+    $tambahstokkc = $stokkc + intval($kc);
 
-    $stokbb12 = $ambildatanya['BB12'];
-    $tambahstokbb12 = $stokbb12 - intval($bb12);
+    $cekbb16 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='BB16'");
+    $ambildatanya = mysqli_fetch_array($cekbb16);
+    $stokbb16 = $ambildatanya['stok'];
+    $tambahstokbb16 = $stokbb16 + intval($bb16);
 
-    $stokbb16 = $ambildatanya['BB16'];
-    $tambahstokbb16 = $stokbb16 - intval($bb16);
+    $cekbb12 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='BB12'");
+    $ambildatanya = mysqli_fetch_array($cekbb12);
+    $stokbb12 = $ambildatanya['stok'];
+    $tambahstokbb12 = $stokbb12 + intval($bb12);
 
-    $stokbice = $ambildatanya['BICE16'];
-    $tambahstokbice = $stokbice - intval($bice);
+    $cekbice16 = mysqli_query($koneksi, "SELECT * FROM gudang_besar WHERE id_produk='BB16'");
+    $ambildatanya = mysqli_fetch_array($cekbice16);
+    $stokbice16 = $ambildatanya['stok'];
+    $tambahstokbice16 = $stokbice16 + intval($bice);
 
-    $update = mysqli_query($koneksi, "UPDATE stok_gudang_kecil 
-    SET B20 ='$tambahstokb20',
-        B16 ='$tambahstokb16',
-        B12 ='$tambahstokb12',
-        R16 ='$tambahstokr16',
-        R12 ='$tambahstokr20',
-        KK ='$tambahstokkk',
-        KC ='$tambahstokkc',
-        BB12 ='$tambahstokbb12',
-        BB16 ='$tambahstokbb16',
-        BICE16 ='$tambahstokbice'");
+    $update1 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokb20'  WHERE id_produk='B20'");
+    $update2 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokb16'  WHERE id_produk='B16'");
+    $update3 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokb12'  WHERE id_produk='B12'");
+    $update4 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokr16'  WHERE id_produk='R16'");
+    $update5 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokr12'  WHERE id_produk='R12'");
+    $update6 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokkk'  WHERE id_produk='KK'");
+    $update7 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokkc'  WHERE id_produk='KC'");
+    $update8 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokbb16'  WHERE id_produk='BB16'");
+    $update9 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokbb12'  WHERE id_produk='BB12'");
+    $update10 = mysqli_query($koneksi, "UPDATE gudang_besar SET stok ='$tambahstokbice16'  WHERE id_produk='BICE16'");
 
 
-    if($addtoproduk AND $addmasuk AND $update){
+    if($update1 AND $update2 AND $update3 AND $update4 AND $update5 AND $update6 AND $update7 AND $update8 AND $update9 AND $update10){
         echo "
         <script>
             alert('data berhasil di tambahkan!');
-            document.location.href = 'index.php';
+            document.location.href = 'stok_gudang_besar.php';
         </script>
         ";
     }else{
         echo "
         <script>
             alert('data gagal di tambahkan!');
-            document.location.href = 'index.php';
+            document.location.href = 'stok_gudang_besar.php';
         </script>
         ";
     }
 }
 
-//Updtade Stok
-if(isset($_POST['updatestok'])){
+if(isset($_POST['stokgkecil'])){
     $b20 = $_POST["b20"];
     $b16 = $_POST["b16"];
     $b12 = $_POST["b12"];
@@ -179,307 +172,4 @@ if(isset($_POST['updatestok'])){
         ";
     }
 }
-
-// Menambah masuk spo
-//Menambah barang baru
-if(isset($_POST['addspo'])){
-    $namasales = $_POST["namasales"];
-    $namatoko = $_POST["toko"];
-    $distrik = $_POST["distrik"];
-    $routing = $_POST["routing"];
-    $b20 = $_POST["b20"];
-    $b16 = $_POST["b16"];
-    $b12 = $_POST["b12"];
-    $r16 = $_POST["r16"];
-    $r12 = $_POST["r12"];
-    $kk = $_POST["kk"];
-    $kc = $_POST["kc"];
-    $bb16 = $_POST["bb16"];
-    $bb12 = $_POST["bb12"];
-    $bice = $_POST["bice"];
-    $keterangan = $_POST["keterangan"];
-    $latitude = $_POST["latitude"];
-    $longitude = $_POST["longitude"];
-
-    $addtoproduk = mysqli_query($koneksi, "INSERT INTO produk VALUES ('$b20', '$b16', '$b12', '$r16', '$r12', '$kk', '$kc', '$bb16', '$bb12', '$bice','','')");
-
-    $getidjual = mysqli_query($koneksi, "SELECT kd_jual FROM produk ORDER BY kd_jual DESC");
-    $rowidjual = mysqli_fetch_array($getidjual);
-    $idjual = $rowidjual['kd_jual'];
-
-    $folder = './produk/';
-    $name_p = $_FILES['foto']['name'];
-    $sumber_p = $_FILES['foto']['tmp_name'];
-    move_uploaded_file($sumber_p, $folder.$name_p);
-
-    // Update tanggal
-    date_default_timezone_set('Asia/Jakarta');
-    $tanggalmasuk = date('Y-m-d H:i:s');
-
-    $tanggalakhir = date('Y-m-d H:i:s', strtotime('+15 days', strtotime($tanggalmasuk)));
-
-    // update status
-    // Tanggal target
-    $targetDate = $tanggalakhir;
-
-    // Waktu Unix dari tanggal target
-    $targetTime = strtotime($targetDate);
-
-    // Selisih waktu antara sekarang dan tanggal target dalam detik
-    $timeDiff = $targetTime - time();
-
-    // Konversi selisih waktu ke jumlah hari
-    $days = round($timeDiff / (60 * 60 * 24));
-    $addmasuk = mysqli_query($koneksi, "INSERT INTO masuk_spo VALUES ('', '$namasales', '$idjual', '$namatoko', '$distrik', '$routing','$tanggalmasuk', '$tanggalakhir', '$keterangan', '$name_p', '$latitude', '$longitude')");
-
-    // update stok
-    $cekstok = mysqli_query($koneksi, "SELECT * FROM stok_gudang_kecil");
-    $ambildatanya = mysqli_fetch_array($cekstok);
-    $stokb20 = $ambildatanya['B20'];
-    $tambahstokb20 = $stokb20 - intval($b20);
-
-    $stokb16 = $ambildatanya['B16'];
-    $tambahstokb16 = $stokb16 - intval($b16);
-
-    $stokb12 = $ambildatanya['B12'];
-    $tambahstokb12 = $stokb12 - intval($b12);
-
-    $stokr16 = $ambildatanya['R16'];
-    $tambahstokr16 = $stokr16 - intval($r16);
-
-    $stokr20 = $ambildatanya['R12'];
-    $tambahstokr20 = $stokr20 - intval($r12);
-
-    $stokkk = $ambildatanya['KK'];
-    $tambahstokkk = $stokkk - intval($kk);
-
-    $stokkc = $ambildatanya['KC'];
-    $tambahstokkc = $stokkc - intval($kc);
-
-    $stokbb12 = $ambildatanya['BB12'];
-    $tambahstokbb12 = $stokbb12 - intval($bb12);
-
-    $stokbb16 = $ambildatanya['BB16'];
-    $tambahstokbb16 = $stokbb16 - intval($bb16);
-
-    $stokbice = $ambildatanya['BICE16'];
-    $tambahstokbice = $stokbice - intval($bice);
-
-    $update = mysqli_query($koneksi, "UPDATE stok_gudang_kecil 
-    SET B20 ='$tambahstokb20',
-        B16 ='$tambahstokb16',
-        B12 ='$tambahstokb12',
-        R16 ='$tambahstokr16',
-        R12 ='$tambahstokr20',
-        KK ='$tambahstokkk',
-        KC ='$tambahstokkc',
-        BB12 ='$tambahstokbb12',
-        BB16 ='$tambahstokbb16',
-        BICE16 ='$tambahstokbice'");
-
-
-    if($addtoproduk AND $addmasuk AND $update){
-        echo "
-        <script>
-            alert('data berhasil di tambahkan!');
-            document.location.href = 'pembelianspo.php';
-        </script>
-        ";
-    }else{
-        echo "
-        <script>
-            alert('data gagal di tambahkan!');
-            document.location.href = 'pembelianspo.php';
-        </script>
-        ";
-    }
-}
-
-
-//Update SPO
-if(isset($_POST['editspo'])){
-    $b20 = $_POST["b20"];
-    $b16 = $_POST["b16"];
-    $b12 = $_POST["b12"];
-    $r16 = $_POST["r16"];
-    $r12 = $_POST["r12"];
-    $kk = $_POST["kk"];
-    $kc = $_POST["kc"];
-    $bb16 = $_POST["bb16"];
-    $bb12 = $_POST["bb12"];
-    $bice = $_POST["bice"];
-    $keterangan = $_POST["keterangan"];
-    $latitude = $_POST["latitude"];
-    $longitude = $_POST["longitude"];
-    $kdspo = $_POST["kdspo"];
-    $kdproduk = $_POST["kdproduk"];
-
-    $update = mysqli_query($koneksi, "UPDATE produk 
-    SET B20 ='$b20',
-        B16 ='$b16',
-        B12 ='$b12',
-        R16 ='$r16',
-        R12 ='$r12',
-        KK ='$kk',
-        KC ='$kc',
-        BB12 ='$bb12',
-        BB16 ='$bb16',
-        BICE16 ='$bice'WHERE kd_jual = '$kdproduk';");
-
-
-    // update stok
-    $cekstok = mysqli_query($koneksi, "SELECT * FROM stok_gudang_kecil");
-    $cekproduk = mysqli_query($koneksi, "SELECT * FROM produk WHERE kd_jual = '$kdproduk';");
-    $ambildatanya = mysqli_fetch_array($cekstok);
-    $ambilproduk = mysqli_fetch_array($cekproduk);
-    $stokb20 = $ambildatanya['B20'];
-    $produkb20 = $ambilproduk['B20'];
-    $tambahstokb20 = $stokb20 - $produkb20;
-
-    $stokb16 = $ambildatanya['B16'];
-    $produkb16 = $ambilproduk['B16'];
-    $tambahstokb16 = $stokb16 - $produkb16;
-
-    $stokb12 = $ambildatanya['B12'];
-    $produkb12 = $ambilproduk['B12'];
-    $tambahstokb12 = $stokb12 - $produkb12;
-
-    $stokr16 = $ambildatanya['R16'];
-    $produkr16 = $ambilproduk['R16'];
-    $tambahstokr16 = $stokr16 - $produkr16;
-
-    $stokr12 = $ambildatanya['R12'];
-    $produkr12 = $ambilproduk['R12'];
-    $tambahstokr12 = $stokr12 - $produkr12;
-
-    $stokkk = $ambildatanya['KK'];
-    $produkkk = $ambilproduk['KK'];
-    $tambahstokkk = $stokkk - $produkkk;
-
-    $stokkc = $ambildatanya['KC'];
-    $produkkc = $ambilproduk['KC'];
-    $tambahstokkc = $stokkc - $produkkc;
-
-    $stokbb12 = $ambildatanya['BB12'];
-    $produkbb12 = $ambilproduk['BB12'];
-    $tambahstokbb12 = $stokbb12 - $produkbb12;
-
-    $stokbb16 = $ambildatanya['BB16'];
-    $produkbb16 = $ambilproduk['BB16'];
-    $tambahstokbb16 = $stokbb16 - $produkbb16;
-
-    $stokbice = $ambildatanya['BICE16'];
-    $produkbice = $ambilproduk['BICE16'];
-    $tambahstokbice = $stokbice - $produkbice;
-
-    $update2 = mysqli_query($koneksi, "UPDATE masuk_spo 
-    SET keterangan = '$keterangan',
-        latitude = '$latitude',
-        longitude = '$longitude'
-        WHERE kd_masuk_spo = '$kdspo';");
-
-    $update3 = mysqli_query($koneksi, "UPDATE stok_gudang_kecil
-    SET B20 ='$tambahstokb20',
-        B16 ='$tambahstokb16',
-        B12 ='$tambahstokb12',
-        R16 ='$tambahstokr16',
-        R12 ='$tambahstokr12',
-        KK ='$tambahstokkk',
-        KC ='$tambahstokkc',
-        BB12 ='$tambahstokbb12',
-        BB16 ='$tambahstokbb16',
-        BICE16 ='$tambahstokbice';");
-
-    
-    if($update AND $update2 AND $update3){
-        echo "
-        <script>
-            alert('data berhasil di tambahkan!');
-            document.location.href = 'pembelianspo1.php';
-        </script>
-        ";
-    }else{
-        echo "
-        <script>
-            alert('data gagal di tambahkan!');
-            document.location.href = 'pembelianspo1.php';
-        </script>
-        ";
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Menambah barang baru sales
-// if(isset($_POST['addnewsales'])){
-//     $namasales = $_POST["namasales"];
-//     $namatoko = $_POST["toko"];
-//     $distrik = $_POST["distrik"];
-//     $routing = $_POST["routing"];
-//     $jkunjungan = $_POST["jkunjungan"];
-//     $b20 = $_POST["b20"];
-//     $b16 = $_POST["b16"];
-//     $b12 = $_POST["b12"];
-//     $r16 = $_POST["r16"];
-//     $r12 = $_POST["r12"];
-//     $kk = $_POST["kk"];
-//     $kc = $_POST["kc"];
-//     $bb16 = $_POST["bb16"];
-//     $bb12 = $_POST["bb12"];
-//     $bice = $_POST["bice"];
-//     $keterangan = $_POST["keterangan"];
-//     // $geocode = $_POST["geocode"];
-//     $latitude = $_POST["latitude"];
-//     $longitude = $_POST["longitude"];
-
-
-//     $folder = './produk/';
-//     $name_p = $_FILES['foto']['name'];
-//     $sumber_p = $_FILES['foto']['tmp_name'];
-//     move_uploaded_file($sumber_p, $folder.$name_p);
-//     $addtotoko = mysqli_query($koneksi, "INSERT INTO toko VALUES ('','$namatoko', '$distrik', '$routing', '$jkunjungan', '$keterangan', '$name_p', '$latitude', '$longitude')");
-
-//     $addtoproduk = mysqli_query($koneksi, "INSERT INTO produk VALUES ('$b20', '$b16', '$b12', '$r16', '$r12', '$kk', '$kc', '$bb16', '$bb12', '$bice','')");
-
-//     $getidjual = mysqli_query($koneksi, "SELECT kd_jual FROM produk ORDER BY kd_jual DESC");
-//     $rowidjual = mysqli_fetch_array($getidjual);
-//     $idjual = $rowidjual['kd_jual'];
-
-//     date_default_timezone_set('Asia/Jakarta');
-//     $tanggal = date('Y-m-d H:i:s');
-//     $addmasuk = mysqli_query($koneksi, "INSERT INTO masuk_toko VALUES ('', '$namasales', '$idjual', '$namatoko', '$distrik', '$routing','$tanggal''$jkunjungan', '$keterangan', '$name_p', '$latitude', '$longitude')");
-
-//     if($addtotoko AND $addtoproduk AND $addmasuk){
-//         echo "
-//         <script>
-//             alert('data berhasil di tambahkan!');
-//             document.location.href = 'sales.php';
-//         </script>
-//         ";
-//     }else{
-//         echo "
-//         <script>
-//             alert('data gagal di tambahkan!');
-//             document.location.href = 'sales.php';
-//         </script>
-//         ";
-//     }
-// }
-
 ?>
